282bfded0f0f5acd651343ac44b7449267631ea8
aesir_glb <aesir1995@outlook.com>
2019-04-20 15:29:53 +0800

    [Newly increased Node 02 & 03 of Eureka Cluster]

---
+++ Eureka Cluster/Eureka 02/src/main/java/eureka/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package eureka;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @ProjectName: Eureka 01
+ * @Description: Cancel the default CSRF validation of Spring Security
+ * @Author: Aesir
+ * @Date: 2019/4/20 0:11
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
